9678fda4984f0da5c0d1e490cf85785895b8f9a8
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-05-23 13:03:56 +0530

    Navbar basic functionality added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
   title: "UrbanScape",
@@ -12,7 +13,8 @@ const MainLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div>{children}</div>
+        <Navbar />
+        <main>{children}</main>
       </body>
     </html>
   );
