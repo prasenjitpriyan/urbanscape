2169fcb05968df52ca2982a0787d6958e6de1bf5
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-05-24 22:27:28 +0530

    New route added

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,7 +1,71 @@
+"use client";
 import React from "react";
+import Link from "next/link";
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import { fetchProperty } from "@/utils/requests";
+import { FaArrowLeft } from "react-icons/fa";
+import PropertyDetails from "@/components/PropertyDetails";
 
 const PropertyPage = () => {
-  return <div>PropertyPage</div>;
+  const { id } = useParams();
+  const [property, setProperty] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPropertyData = async () => {
+      if (!id) return;
+      try {
+        const property = await fetchProperty(id);
+        setProperty(property);
+      } catch (error) {
+        console.error("Error fetching property:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (property === null) {
+      fetchPropertyData();
+    }
+  }, [id, property]);
+
+  if (!property && !loading) {
+    return (
+      <h1 className="text-center text-2xl font-bold mt-10">
+        Property Not Found
+      </h1>
+    );
+  }
+
+  return (
+    <React.Fragment>
+      {!loading && property && (
+        <>
+          <PropertyHeaderImage image={property.images[0]} />
+          <section>
+            <div className="container m-auto py-6 px-6">
+              <Link
+                href="/properties"
+                className="text-blue-500 hover:text-blue-600 flex items-center"
+              >
+                <FaArrowLeft className="mr-2" /> Back to Properties
+              </Link>
+            </div>
+          </section>
+          <section className="bg-blue-50">
+            <div className="container m-auto py-10 px-6">
+              <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+                <PropertyDetails property={property} />
+                <aside className="space-y-4"></aside>
+              </div>
+            </div>
+          </section>
+        </>
+      )}
+    </React.Fragment>
+  );
 };
 
 export default PropertyPage;
--- utils/requests.js
+++ utils/requests.js
@@ -17,4 +17,25 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties };
+// Fetch single property
+async function fetchProperty(id) {
+  try {
+    // Handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return null;
+    }
+
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };
