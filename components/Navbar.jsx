3dc60fb0d50de04a33cecdcb788384b9844a28ed
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-05-25 20:11:01 +0530

    Sign out and protects routes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -190,6 +190,7 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-0"
+                      onClick={() => setIsMobileMenuOpen(false)}
                     >
                       Your Profile
                     </Link>
@@ -199,10 +200,15 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-2"
+                      onClick={() => setIsMobileMenuOpen(false)}
                     >
                       Saved Properties
                     </Link>
                     <button
+                      onClick={() => {
+                        setIsMobileMenuOpen(false);
+                        signOut();
+                      }}
                       className="block px-4 py-2 text-sm text-gray-700"
                       role="menuitem"
                       tabIndex="-1"
