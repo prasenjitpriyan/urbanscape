9d79ce43ed9ac62121619b9530af8461b5bfc57b
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-05-27 23:20:26 +0530

    Featured router and component added

--- app/page.jsx
+++ app/page.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
 import HomeProperties from "@/components/HomeProperties";
+import FeaturedProperties from "@/components/FeaturedProperties";
 
 const HomePage = () => {
   return (
     <React.Fragment>
       <Hero />
       <InfoBoxes />
+      <FeaturedProperties />
       <HomeProperties />
     </React.Fragment>
   );
--- utils/requests.js
+++ utils/requests.js
@@ -1,18 +1,21 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
-async function fetchProperties() {
+async function fetchProperties({ showFeatured = false } = {}) {
   try {
     // Handle the case where the domain is not available yet
     if (!apiDomain) {
       return [];
     }
-    const response = await fetch(`${apiDomain}/properties`, {
-      cache: "no-store",
-    });
-    if (!response.ok) {
+    const res = await fetch(
+      `${apiDomain}/properties${showFeatured ? "/featured" : ""}`,
+      { cache: "no-store" }
+    );
+
+    if (!res.ok) {
       throw new Error("Failed to fetch data");
     }
-    return response.json();
+
+    return res.json();
   } catch (error) {
     console.log(error);
     return [];
